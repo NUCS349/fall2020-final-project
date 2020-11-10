6e3c9a452318887c15d718f8652d0bcaaf4401b5
Mike D'Arcy <m.m.darcy@u.northwestern.edu>
2020-11-10 12:10:29 -0600

    Add BAG test

--- tests/test_text_classification.py
+++ tests/test_text_classification.py
@@ -9,11 +9,26 @@ def test_dbpedia():
     train_exs, dev_exs, test_exs = load_data('data/dbpedia_small')
 
     model = TextClassificationModel()
+    model.train([x['text'] for x in train_exs], [int(x['label'])-1 for x in train_exs])
+
+    preds = model.predict([x['text'] for x in test_exs])
+    accuracy = sum((1 if pred == int(x['label'])-1 else 0) for pred, x in zip(preds, test_exs)) / len(test_exs)
+
+    print('accuracy', accuracy)
+    assert accuracy > 0.96
+
+def test_bag():
+    np.random.seed(0)
+    rng = np.random.default_rng(0)
+
+    train_exs, dev_exs, test_exs = load_data('data/bag')
+
+    model = TextClassificationModel()
     model.train([x['text'] for x in train_exs], [int(x['label']) for x in train_exs])
 
     preds = model.predict([x['text'] for x in test_exs])
     accuracy = sum((1 if pred == int(x['label']) else 0) for pred, x in zip(preds, test_exs)) / len(test_exs)
 
     print('accuracy', accuracy)
-    assert accuracy > 0.96
+    assert accuracy > 0.70
 
